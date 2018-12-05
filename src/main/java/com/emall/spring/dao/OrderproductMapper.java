6a10d3a592d673263980f8e5dfab9a2c05fbb57d
Mingmingge <hlm_hulimin@163.com>
2018-12-05 13:54:17 +0800

    修改顾客有关接口

--- src/main/java/com/emall/spring/dao/OrderproductMapper.java
+++ src/main/java/com/emall/spring/dao/OrderproductMapper.java
@@ -3,6 +3,8 @@ package com.emall.spring.dao;
 import com.emall.spring.entity.Orderproduct;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface OrderproductMapper {
     int deleteByPrimaryKey(String uuid);
@@ -16,4 +18,6 @@ public interface OrderproductMapper {
     int updateByPrimaryKeySelective(Orderproduct record);
 
     int updateByPrimaryKey(Orderproduct record);
+
+    ArrayList<Orderproduct> selectByOrderid(String orderid);
 }
