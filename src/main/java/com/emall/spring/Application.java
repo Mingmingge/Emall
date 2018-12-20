e43fb08fd3cc216871368d1a0928d5a702a6eb6c
leijunjie <1016001532@qq.com>
2018-12-20 12:51:19 +0800

    lei

--- src/main/java/com/emall/spring/Application.java
+++ src/main/java/com/emall/spring/Application.java
@@ -3,17 +3,19 @@ package com.emall.spring;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @MapperScan("com.emall.spring.dao")
+@ServletComponentScan  //springboot启动类扫描servlet组件(过滤器)
 public class Application {
-
 
     /**
      * 测试
+     *
      * @return 测试用
      */
 
@@ -22,7 +24,7 @@ public class Application {
         return "ok!";
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
