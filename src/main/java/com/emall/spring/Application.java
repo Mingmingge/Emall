98f8571ac642d27582ecbe8ac915ef6518acefb7
leijunjie <1016001532@qq.com>
2018-12-21 12:10:48 +0800

    lei

--- src/main/java/com/emall/spring/Application.java
+++ src/main/java/com/emall/spring/Application.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @MapperScan("com.emall.spring.dao")
-@ServletComponentScan  //springboot启动类扫描servlet组件(过滤器)
+//@ServletComponentScan  //springboot启动类扫描servlet组件(过滤器)
 public class Application {
 
 
